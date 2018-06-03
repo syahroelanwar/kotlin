6b2f725fb6f525877ce471b638109de66b1440c9
syahroelanwar <syahrul.am9773@gmail.com>
2018-06-03 13:52:51 +0700

    mdc-android-kotlin

---
+++ app/src/main/java/com/gmail/am9773/syahrul/workshop8app/JsonReader.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gmail.am9773.syahrul.workshop8app
+
+import com.google.gson.Gson
+import java.io.IOException
+import java.io.InputStream
+import java.lang.reflect.Type
+
+/**
+ * Utilities for reading JSON input into Java objects.
+ */
+object JsonReader {
+    private val TAG = JsonReader::class.java.simpleName
+
+    @Throws(IOException::class)
+    fun <T> readJsonStream(inputStream: InputStream, typeOfT: Type): T {
+        val jsonString = inputStream.bufferedReader().use{ it.readText() }
+        val gson = Gson()
+        return gson.fromJson<T>(jsonString, typeOfT)
+    }
+}
---
+++ app/src/main/java/com/gmail/am9773/syahrul/workshop8app/ProductEntry.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gmail.am9773.syahrul.workshop8app
+
+/**
+ * A product entry in the list of products.
+ */
+class ProductEntry(val title: String, val url: String, val year: String, val description: String)
